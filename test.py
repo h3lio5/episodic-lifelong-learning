41f75ececb48ed5e9ff577cd26acdb1316e452da
h3li05369 <akashsm@ce.iitr.ac.in>
2019-10-22 23:01:22 +0530

    path to data corrected

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 for i in range(4):
-    df = pd.read_csv('data/ordered_data/test/' + str(i+1) +'.csv')
+    df = pd.read_csv('../data/ordered_data/train/' + str(i+1) +'.csv')
     print(df)
     df.dropna(subset=['content'],inplace=True)
     print(df)
-    df.to_csv('data/ordered_data/test/' + str(i+1) +'.csv',index=False)
+    df.to_csv('../data/ordered_data/train/' + str(i+1) +'.csv',index=False)
