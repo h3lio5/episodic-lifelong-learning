d7be3b26e2a4018e7061da780f85dd0891f489c6
h3li05369 <akashsm@ce.iitr.ac.in>
2019-11-02 01:11:21 +0530

    updated replay model

--- baselines/replay.py
+++ baselines/replay.py
@@ -1,5 +1,6 @@
 import torch
 import random
+import transformers 
 
 
 
@@ -14,6 +15,20 @@ class Replay(object):
         Initialize the replay buffer
         """
         self.memory = []
+        # Initialize the pretrained bert model for sequence classification
+        self.classifier = transformers.BertForSequenceClassification.from_pretrained('bert-base-uncased',num_labels=33)
+
+    def classify(self,content,attn_masks,labels):
+
+        self.classifier.train()
+        loss,logits = self.classifier(content,attention_mask=attn_masks,labels=labels)    
+        return loss,logits
+
+    def infer(self,content,attn_masks):
+
+        self.classifier.eval()
+        logits = self.classifier(content,attention_mask=attn_masks)
+        return logits
 
     def push(self,examples):
         """
