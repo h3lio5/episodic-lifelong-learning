e02a842e7ba59cfe37eda56279446b590fe9c379
h3li05369 <akashsm@ce.iitr.ac.in>
2019-11-05 02:10:27 +0530

    accuracy metric changed

--- main.py
+++ main.py
@@ -85,10 +85,10 @@ def train(order,model):
     save_trainloss(train_loss_set)    
 
 # Function to calculate the accuracy of our predictions vs labels
-def num_correct(preds, labels):
+def flat_accuracy(preds, labels):
     pred_flat = np.argmax(preds, axis=1).flatten()
     labels_flat = labels.flatten()
-    return np.sum(pred_flat == labels_flat) 
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
 
 def test(order,model):
     """
