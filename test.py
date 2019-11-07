bc125481d9f91d1b5d32a5007b06cc8f33fcd163
h3li05369 <akashsm@ce.iitr.ac.in>
2019-11-07 22:30:56 +0530

    test.py changed

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-import transformers 
+import transformers
 import torch
 
-model = transformers.BertForSequenceClassification.from_pretrained('bert-base-uncased',num_labels=33)
+model = transformers.BertForSequenceClassification.from_pretrained(
+    'bert-base-uncased', num_labels=33)
 tokenizer = transformers.BertTokenizer.from_pretrained('bert-base-uncased')
-input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute")).unsqueeze(0)  # Batch size 1
+input_ids = torch.tensor(tokenizer.encode(
+    "Hello, my dog is cute")).unsqueeze(0)  # Batch size 1
 logits, = model(input_ids)
 print(logits)
 print(type(logits))
