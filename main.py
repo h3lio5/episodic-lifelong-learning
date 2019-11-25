1d3385471f3ccefd1c3a55523d5ff093c1159615
h3li05369 <akashsm@ce.iitr.ac.in>
2019-11-25 17:45:59 +0530

    minor change

--- main.py
+++ main.py
@@ -167,6 +167,6 @@ if __name__ == '__main__':
 
     if args.mode == 'test':
         model_state = torch.load(
-            '../model_checkpoints/enc_dec/classifier_order_1_epoch_3.pth')
+            '../model_checkpoints/enc_dec/classifier_order_1_epoch_2.pth')
         model = EncDec(mode='test', model_state=model_state)
         test(args.order, model)
